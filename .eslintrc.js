d99022aaa762692b28fc78c12b3d9dd48c840b4e
leigp <907090585@qq.com>
2022-05-08 21:14:09 +0800

    docs: 测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,33 @@
+//.eslinc.js
 module.exports = {
-    'root': true,
-    'env': {
-        'browser': true,
-        'node': true,
-        'es6': true,
-        "vue/setup-compiler-macros":true
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
     },
-    'extends': [
-        'airbnb-base',
+    extends: [
         'plugin:vue/vue3-essential',
-        'plugin:vue/vue3-recommended',
-        'plugin:@typescript-eslint/recommended'
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        // '@vue/prettier/@typescript-eslint',
     ],
-    "parser": "vue-eslint-parser",
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'parser': '@typescript-eslint/parser',
-        'sourceType': 'module'
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
     },
-    'plugins': [
-        'vue',
-        '@typescript-eslint'
-    ],
-    'rules': {
-        'import/no-unresolved': 'off',
-        'import/extensions': 'off',
-        'import/no-absolute-path': 'off',
-        'import/no-extraneous-dependencies': 'off',
-        'vue/no-multiple-template-root': 'off',
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // 是否使用 console
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // 是否使用 debugger
-        'vue/comment-directive': 0,
-        'comma-style': ['error', 'last'], // 逗号风格，换行时在行首还是行尾
-        "vue/max-attributes-per-line": 'off',
-        "vue/html-self-closing":'off',
-        "no-param-reassign":'off',
-        "vue/multi-word-component-names":'off',
-    }
-};
+    rules: {
+        'prettier/prettier': 'error',
+        '@typescript-eslint/no-explicit-any': ['off'],
+        '@typescript-eslint/indent': ['off'],
+        '@typescript-eslint/no-unused-vars': ['error'],
+    },
+    globals: {
+        defineProps: 'readonly',
+        defineEmits: 'readonly',
+        defineComponent: 'readonly',
+        defineExpose: 'readonly',
+    },
+}
+
